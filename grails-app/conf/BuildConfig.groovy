13d6c8ae480da9d08d3234c40b7523dedf6e8eea
leftovers <tyama@xmldo.net>
2010-08-22 14:46:27 +0900

    pluginsとworkを別ディレクトリへ

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,8 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.plugins.dir="work_tmp/plugins"
+grails.project.work.dir="work_tmp/work"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
